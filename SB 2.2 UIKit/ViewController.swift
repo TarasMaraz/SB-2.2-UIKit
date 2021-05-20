586c631116199d7e3538787902830e80b3855855
TarasMaraz <taras.panin@gmail.com>
2021-05-20 21:38:08 +0300

    Initial Commit

---
+++ SB 2.2 UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB 2.2 UIKit
+//
+//  Created by Тарас Панин on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
